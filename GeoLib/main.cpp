0a6dcfd5d157b65776eab54bf096e56e75099945
asaren08 <bontendou@gmail.com>
2018-07-08 03:00:08 +0900

    Fix main function.

--- GeoLib/main.cpp
+++ GeoLib/main.cpp
@@ -1,15 +1,6 @@
-//
-//  main.cpp
-//  GeoLib
-//
-//  Created by Renshi Asada on 2018/07/08.
-//  Copyright © 2018年 Renshi Asada. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    // write your code
     return 0;
 }
