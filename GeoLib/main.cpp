b2883c62b8f1133cd319c83008c8c01e48b82cfa
asaren08 <bontendou@gmail.com>
2018-07-08 02:49:41 +0900

    Initial Commit

---
+++ GeoLib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GeoLib
+//
+//  Created by Renshi Asada on 2018/07/08.
+//  Copyright © 2018年 Renshi Asada. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
